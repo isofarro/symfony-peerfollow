a5d53f51fb6e2c24b8ec0ed4adedf37511a54c30
Isofarro <isofarro2@gmail.com>
2009-05-09 14:33:14 +0100

    local modifications plus database details

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '/usr/share/pear/symfony/autoload/sfCoreAutoload.class.php';
+require_once '/usr/share/php/symfony/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
