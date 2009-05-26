c517eda0988199ee9e58751ae05936a30eff6d3b
Isofarro <isofarro2@gmail.com>
2009-05-26 18:40:15 +0100

    Creating a community data object with helper methods

--- lib/Community.php
+++ lib/Community.php
@@ -1,9 +1,12 @@
 <?php
 
 class Community {
-	var $people;
-	var $connections;
+	static $FOLLOWER  = 1;
+	static $FOLLOWING = 2;
 
+	var $name;
+	var $people;
+	var $connections = array();
 
 	public function isFollowing($person1, $person2) {
 	
@@ -17,6 +20,30 @@ class Community {
 	
 	}
 
+
+	protected function addRelationship($person1, $person2, $relationship) {
+		switch($relationship) {
+			case self::FOLLOWER:
+				$this->connectFollower($person1, $person2);
+				break;
+			case self::FOLLOWING:
+				$this->connectFollower($person2, $person1);
+				break;
+			case self::FRIEND:
+				$this->connectFollower($person1, $person2);
+				$this->connectFollower($person2, $person1);
+				break;
+			default:
+				break;
+		}
+	}
+	
+	protected function connectFollower($person1, $person2) {
+		if (empty($this->connections[$person1])) {
+			$this->connections[$person1] = array();
+		}
+		$this->connections[$person1][$person2] = 1;
+	}
 }
 
 ?>
